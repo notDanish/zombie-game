c439971c7d16c63144b1938adc7f6c92b5564a9e
notDanish <anishkumar.business@gmail.com>
2016-05-09 19:43:12 -0400

    game project created

---
+++ Game/Assets/MyVRButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyVRButton : MonoBehaviour {
+	public string triggerCall;
+
+	void Start() {
+	}
+
+	public void OnGazeEnter() {
+		Debug.Log ("HIII");
+	}
+
+	public void OnGazeExit() {
+		Debug.Log ("BYEE");
+	}
+
+	public void trigger() {
+		Debug.Log ("YOLO");
+		if (triggerCall.Length == 0) {
+			
+		} else {
+			GetComponent<Animator> ().Play (triggerCall);
+		}
+	}
+}
---
+++ Game/Assets/zombieWalk.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zombieWalk : MonoBehaviour {
+
+	public string zombieMotion;
+
+	// Use this for initialization
+	void Start () {
+		if (zombieMotion.Length > 0) {
+			this.GetComponent<Animator> ().Play (zombieMotion);
+		} else {
+			this.GetComponent<Animator> ().Play ("attack");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
